7ceffa1d35b4bfb944cb0ced08f205fb16a6d21a
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-02-27 18:33:41 +0100

    666

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,11 @@
 
 const textInput = document.querySelector("#validation-input");
 textInput.addEventListener("blur", (event) => {
-    currentTarget.classList.remove("valid", "invalid");
     if (event.currentTarget.value.length === Number(textInput.dataset.length)) {
+        event.currentTarget.classList.remove("invalid");
         event.currentTarget.classList.add("valid");
     } else {
+        event.currentTarget.classList.remove("valid");
         event.currentTarget.classList.add("invalid");
     }
 });

265fab247e4d0182634ce4592bf83273be776044
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-02-27 18:15:45 +0100

    888

--- js/task-06.js
+++ js/task-06.js
@@ -2,6 +2,7 @@
 
 const textInput = document.querySelector("#validation-input");
 textInput.addEventListener("blur", (event) => {
+    currentTarget.classList.remove("valid", "invalid");
     if (event.currentTarget.value.length === Number(textInput.dataset.length)) {
         event.currentTarget.classList.add("valid");
     } else {
--- js/task-08.js
+++ js/task-08.js
@@ -13,6 +13,6 @@ function handleSubmit(event) {
     return alert("Please fill in all the fields!");
   }
 
-  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  console.log(`login: ${email.value}, password: ${password.value}`);
   event.currentTarget.reset();
 }
